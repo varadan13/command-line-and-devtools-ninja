d551fd70e4cd1dde7f14abb30f54460d2d04a6d9
Aneesh Varadan <rcaesar1996@gmail.com>
2023-09-15 14:42:56 +0530

    Create load-script-into-devtools.js

---
+++ load-script-into-devtools.js
@@ -0,0 +1,4 @@
+var script = document.createElement('script');
+script.src = 'https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.9.0/html2pdf.bundle.min.js';
+script.type = 'text/javascript';
+document.head.appendChild(script);
